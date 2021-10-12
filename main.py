11505fb3a6560f8c0f73a515efbe69d149a5f6ec
Caspermun <caspermunele@gmail.com>
2021-10-12 20:43:34 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+dateOfBirthday = 2001  # Camel case
+date_of_birthday = 2001  # Snake case
+var_int = 1  # Variable integer (число)
+var_float = 1.2  # Variable float (числа с плавающей точкой)
+var_str = "Caspermun"  # Variable string (строка)
+var_boolean_true = True  # Variable boolean (истина)
+var_boolean_false = False  # Variable boolean (ложь)
+var_list = [1, 2, 3, "Caspermun",[1, 2, 3], (1, 2, 3), {"name": "Caspermun"}]  # Variable list (список)
+var_tuple = (1, [1, 2], "Caspermun",)  # Variable tuple (кортеж)
+var_dict = {"name": "Caspermun", "date_of_birthday": "25.04.2001", "course": 3,
+            "physical_data": [50, 170], "python": True}  # Variable dictionary (словарь)
+
+
+def print_hi():
+    dateOfBirthday = 1996  # Camel case
+    date_of_birthday = 1996  # Snake case
+    var_int = 18  # Variable integer (число)
+    var_float = 4.5  # Variable float (числа с плавающей точкой)
+    var_str = "Nursultan"  # Variable string (строка)
+    var_boolean_true = True  # Variable boolean (истина)
+    var_boolean_false = False  # Variable boolean (ложь)
+    var_list = [4, 5, 6, "Nursultan", [4, 5, 6], (4, 5, 5), {"name": "Nursultan"}]  # Variable list (список)
+    var_tuple = (4, [4, 5], "Nursultan",)  # Variable tuple (кортеж)
+    var_dict = {"name": "Nursultan", "date_of_birthday": "29.05.1996", "course": 5,
+                "physical_data": [70, 190], "python": True}  # Variable dictionary (словарь)
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('var_int')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
